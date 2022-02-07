5c3005d8e870594a15e10c3127f67c23878af740
Keith Kerswell <keithkerswell@outlook.com>
2022-02-07 21:58:59 +0000

    Function to fade out any element with class="tag" on sroll down and then fade back in on scroll up by adding .hidden css style
    Not working fully as intended currently.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 // function to add active class to nav link at top of page
 // Source code https://stackoverflow.com/a/26383047
 $(window).scroll(function(){
-    var scrollTop = $(document).scrollTop();
+    let scrollTop = $(document).scrollTop();
     
     // Changed source code to use OR in the selector
     // Source code https://stackoverflow.com/a/10687171
-    var anchors = $('main').find('[id="intro"],[id="howtoplay"],[id="game"]');
+    let anchors = $('main').find('[id="intro"],[id="howtoplay"],[id="game"]');
     
-    for (var i = 0; i < anchors.length; i++){
+    for (let i = 0; i < anchors.length; i++){
         if (scrollTop > $(anchors[i]).offset().top && scrollTop < $(anchors[i]).offset().top + $(anchors[i]).height()) {
             $('.head_links li a[href="#' + $(anchors[i]).attr('id') + '"]').addClass('active');
         } else {
@@ -15,3 +15,27 @@ $(window).scroll(function(){
         }
     }
 });
+
+// Function to fade out any element with class="tag" on sroll down and then fade back in on scroll up by adding .hidden css style
+$(window).scroll(function() {
+    let pageTop = $(document).scrollTop();
+    let tags = $('main').find('.tag');    
+    
+    for (let i = 0; i < tags.length; i++) {
+        let tag = tags[i];
+
+        if (pageTop > $(tags[i]).offset().top - 50 && pageTop < $(tags[i]).offset().top + $(tags[i]).height() - 50) {
+            $(tag).addClass('hidden');
+        }  
+    }
+
+    for (let i = 0; i < tags.length; i++) {
+        let tagHidden = tags[i];
+
+        if (pageTop < $(tags[i]).offset().top && pageTop > $(tags[i]).offset().top - $(tags[i]).height()) {
+            $(tagHidden).removeClass('hidden');
+        }  
+    }
+    
+});
+ 
